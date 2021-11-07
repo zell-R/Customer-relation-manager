91ae6b19634ed4a515b05eb57774d734c75c2881
zell-R <rlazarte_08@hotmail.com>
2021-11-06 21:36:12 -0500

    created a customer controller, index action/view, alphabetized action/view, missing action/view, and updated the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
+  # resources :customers, only: %i[index]
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "customers#index"
+  # get "/customers", to: "customers#index", as: "customers" # companies_path
+  # get "/customers/alphabetized", to: "customers#alphabetized", as: "acustomers" # companies_path
+  # get "/companies/:id", to: "companies#show", as: "company" # company_path
+  resources :customers do
+    collection do
+      get "alphabetized"
+      get "missing_email"
+    end
+  end
 end
